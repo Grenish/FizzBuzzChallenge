0803e1ea7c0e5e326c5111a969237eb3b52f619d
Grenish rai <ariestheracer@gmail.com>
2023-08-01 21:13:39 +0530

    Javascript

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz(n) {
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+    }
+  }
+  
+  // Test the function for numbers from 1 to 20
+  fizzbuzz(20);
+  
