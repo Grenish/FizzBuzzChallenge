ef9c5028e95373e7358ba579ab4b136ae7a6b4dd
Grenish rai <ariestheracer@gmail.com>
2023-08-01 23:08:25 +0530

    Small Bug Fixed

--- java/fizzbuzz.java
+++ java/fizzbuzz.java
@@ -1,6 +1,6 @@
 package java;
 public class fizzbuzz {
-    public static void fizzbuzz(int n) {
+    public static void FizzBuzz(int n) {
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
@@ -16,6 +16,6 @@ public class fizzbuzz {
 
     public static void main(String[] args) {
         // Test the function for numbers from 1 to 20
-        fizzbuzz(20);
+        FizzBuzz(20);
     }
 }
