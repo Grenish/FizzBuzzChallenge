5511e7c253e11f929161aea5936dbcc4f690dee2
Grenish rai <ariestheracer@gmail.com>
2023-08-01 21:13:28 +0530

    Update in Java

---
+++ java/fizzbuzz.java
@@ -0,0 +1,21 @@
+package java;
+public class fizzbuzz {
+    public static void fizzbuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test the function for numbers from 1 to 20
+        fizzbuzz(20);
+    }
+}
